d411e1f397ce389e9a9e43762006da1bffa17c5f
Katdelorme <katdelorme@yahoo.com>
2017-03-28 09:14:27 -0400

    adding previous night work

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Payment Page | Coded by Katherine Delorme</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="description" content=""/>
+	<meta name="keywords" content="">
+	<meta name="author" content="Katherine Delorme">
+	<meta name="application-name" content="Payment Page - UI to Frontend"/>
+	<!--Dublin Core-->
+	<meta name="DC.Title" content="Payment Page | Coded by Katherine Delorme" />
+	<meta name="DC.Creator" content="Katherine Delorme" />
+	<meta name="DC.Subject" content="Web Design, UI to Frontend Design" />
+	<meta name="DC.Description" content="" />
+	<meta name="DC.Type" content="Portfolio Piece" />
+	<meta name="DC.Language" content="English" />
+	<!--Twitter Card: Summary-->
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@Katdelorme" />
+	<meta name="twitter:title" content="Payment Page - UI to Frontend" />
+	<meta name="twitter:description" content="A website UI mockup turned webpage. A payment page coded by Katherine Delorme. Frontend design utilizing html5, SCSS, Cleave JS, Scout-App, flexbox and more." />
+	<meta name="twitter:image" content="http://katherinedelorme.com/applanding/assets/appLanding.png" />
+	<!--Facebook Open Graph-->
+	<meta property="og:url" content="http://katherinedelorme.com/applanding/index.php" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Payment Page - UI to Frontend" />
+	<meta property="og:description" content="A website UI mockup turned webpage. A payment landing page coded by Katherine Delorme. Frontend design utilizing html5, SCSS, Cleave JS, Scout-App, flexbox and more." />
+	<meta property="og:image" content="http://katherinedelorme.com/applanding/assets/appLanding.png" />
+	<!--favicon-->
+	<link rel="icon" type="image/png" href="favicon/favicon-bars.ico" />
+	<!--stylesheets-->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,700" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+<body>
+<header>
+  <div class="logo">
+    <img src="http://placehold.it/140x30" alt="Logo for the Madari App" />
+  </div>
+  <nav>
+    <i class="fa fa-bars fa-lg" aria-hidden="true"></i>
+  </nav>
+</header>
+
+<section>
+
+</section>
+
+<section>
+
+</section>
+
+<footer>
+  <a href="https://www.facebook.com/KatdelormeKatdesign/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+  <a href="#" target="_blank"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+  <a href="https://twitter.com/Katdelorme" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+  <a href="#" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+  <a href="https://dribbble.com/KatherineDelorme" target="_blank"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+  <p>&copy; 2017 copyright All rights reserved</p>
+  <p>Site mockup by <a href="https://dribbble.com/shots/3336517-Best-App-Landing-page" target="_blank">Josh Paris‎</a> Coded with &hearts; by <a href="https://github.com/Katdelorme/App-Landing-Page" target="_blank">Katherine Delorme</a></p>
+</footer>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js.js"></script>
+
+</body>
+</html>
